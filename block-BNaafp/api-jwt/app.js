2b3f3bfdd514367d63f5d5ba647f05130dbcc17c
Vijay Gupta <vijayg963@gmail.com>
2022-05-28 21:31:13 +0530

    Assignment

--- block-BNaafp/api-jwt/app.js
+++ block-BNaafp/api-jwt/app.js
@@ -3,9 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
+// routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+// connect to the mongoose server
+mongoose.connect('mongodb://localhost/app-jwt', (err) => {
+  console.log(err ? err : 'Connected true');
+});
 
 var app = express();
 
@@ -19,16 +26,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
